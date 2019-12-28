5d45034f78430d016ebca09d8cfce87d12aa2730
dlx24x7 <dlx24x7@gmail.com>
2019-12-28 17:06:42 +0000

    commiting week1_example

---
+++ week1_examples.py
@@ -0,0 +1,28 @@
+"""
+This is my first program 
+Its an example to teach you the fundamentals of python
+Week1_examples.py
+"""
+# Arithmetic expressions - numbers, operators, expressions
+
+print(3, -1, 3.141459, -2.8)
+
+# numbers - two types, an integer or a decimal number
+# two corresponding data types int() and float()
+
+print(type(3), type(3.14159))
+print(type(3.0))
+
+# we can convert between data types using int() and float()
+# note that int() takes the "whole" part of a decimal number
+# float() applied to integers is boring
+
+print(int(3.14159), int(-2.8))
+print(float(3), float(-1))
+
+# floating point number have around 15 decimals of accuracy
+# pi is 3.1415926535897932384626433832795028841971...
+# square root of two is 1.414213562373095048801688724209698
+
+print(3.1415926535897932384626433832795028841971)
+print(1.414213562373095048801688724209698)
